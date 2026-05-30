cd4614d37af10bf72068e17724791bce9c6ca17e
senninsugar <hiroki20134@gmail.com>
2026-05-30 21:14:31 +0900

    Create Register.vue

---
+++ frontend/src/pages/Register.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="page">
+
+```
+<div class="card">
+
+  <h1>
+    新規登録
+  </h1>
+
+  <input
+    v-model="username"
+    placeholder="ユーザー名"
+  >
+
+  <input
+    v-model="password"
+    type="password"
+    placeholder="パスワード"
+  >
+
+  <button
+    @click="submit"
+  >
+    登録
+  </button>
+
+  <p>
+    {{ message }}
+  </p>
+
+</div>
+```
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter }
+from "vue-router";
+import {
+  useAuthStore
+}
+from "../stores/auth";
+
+const auth =
+  useAuthStore();
+
+const router =
+  useRouter();
+
+const username =
+  ref("");
+
+const password =
+  ref("");
+
+const message =
+  ref("");
+
+async function submit() {
+
+  const result =
+    await auth.register(
+      username.value,
+      password.value
+    );
+
+  if (result.success) {
+
+    router.push("/");
+
+  } else {
+
+    message.value =
+      result.message;
+
+  }
+}
+</script>
+
+<style scoped>
+.page{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  min-height:80vh;
+}
+
+.card{
+  width:400px;
+  background:white;
+  padding:20px;
+  border-radius:12px;
+  display:flex;
+  flex-direction:column;
+  gap:12px;
+}
+
+input{
+  padding:12px;
+}
+
+button{
+  padding:12px;
+}
+</style>
