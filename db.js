479f137e7c6cce6c2ebdda6ab9ecb9cf4a53aaf0
senninsugar <hiroki20134@gmail.com>
2026-05-30 21:08:45 +0900

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+import { createClient } from "@libsql/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = createClient({
+url: process.env.TURSO_DATABASE_URL,
+authToken: process.env.TURSO_AUTH_TOKEN
+});
+
+export default db;
